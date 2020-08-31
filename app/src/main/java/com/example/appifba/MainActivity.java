3228882d44c3bed323c6fc3017ae468abd09df69
arnaldocorreiafilho <arnaldocorreiadeandradefilho@gmail.com>
2020-08-31 11:00:59 -0300

    commit inicial.

---
+++ app/src/main/java/com/example/appifba/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.appifba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
