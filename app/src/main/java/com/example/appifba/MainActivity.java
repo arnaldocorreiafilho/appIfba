bf1f8dd26416f872cdc045cee6532a85894fc472
arnaldocorreiafilho <arnaldocorreiadeandradefilho@gmail.com>
2020-09-04 19:53:09 -0300

    commit com as seguintes atividades.
    04/09/2020 Validação do campo denúncia.

--- app/src/main/java/com/example/appifba/MainActivity.java
+++ app/src/main/java/com/example/appifba/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +12,15 @@ import com.example.appifba.dao.DenunciaDao;
 import com.example.appifba.dao.IDenunciaDao;
 import com.example.appifba.model.Denuncia;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
 
     private Button clickButton;
     private EditText text;
+    private final Map<String,Denuncia> denunciaMap = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +35,20 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Denuncia d = new Denuncia();
-                d.setUid("-MGExaSMJA6ZBZm3CU0x");
-                d.setDescricao(text.getText().toString());
-               // dao.salvar(d);
-                dao.atualizar(d);
+
+                if(!text.getText().toString().equals("")) {
+                    Denuncia d = new Denuncia();
+                    //d.setUid("-MGExaSMJA6ZBZm3CU0x");
+                    d.setDescricao(text.getText().toString());
+                    dao.salvar(d);
+                }
+                else {
+
+                    Toast.makeText(v.getContext(), "Valor da Caixa de Texto Vazio",Toast.LENGTH_LONG).show();
+
+                }
+
+
 
             }
         });
